363fae9146e532f9c488368dc88563c201cddc5d
Dmitrykarp <dimankarpenko@yandex.ru>
2015-01-26 23:23:54 +0300

    not use

--- app/src/main/java/bugaga/comsgu/vk/www/guess/MainStart.java
+++ app/src/main/java/bugaga/comsgu/vk/www/guess/MainStart.java
@@ -24,36 +24,32 @@ public class MainStart extends ActionBarActivity {
         bMense=(Button)findViewById(R.id.bLft);
         bBolse=(Button) findViewById(R.id.bRght);
         bGuess=(Button) findViewById(R.id.bGss);
-        textView.setText("Вы загадали " +50 +"?");
-        column=1;
+      //  textView.setText("Вы загадали " +50 +"?");
+        column=0;
         textView2.setText(" " +column +" ");
 
     }
      int x=100;
-     int y=x/2;
-     int a=0; // х - Диапазон,
+     int y=0;
+     int a=0; // х y - Диапазон,
     int column;
     public void setTextView(TextView textView) {
         this.textView = textView;
     }
     public void bMense(View view){
         column++;
-        a=y;
-        if ((x-y)/2!=0) y=y-(x-y)/2; else y=y-1;
-        x=a+1;
-        textView.setText("Вы загадали " +y +"?");
+        a=y+(x-y)/2;
+        textView.setText("Вы загадали " +a +"?");
         textView2.setText(" " +column +" ");
+        x=a-1;
     }
     public void bBolse(View view){
         column++;
 
-        a=y;
-        y=y+(x-y)/2;
-        if(x<y){x=y; y=a+1;}
-
-        textView.setText("Вы загадали " +y +"?");
+        a=y+(x-y)/2;;
+        textView.setText("Вы загадали " +a +"?");
         textView2.setText(" " +column +" ");
-
+        y=a+1;
     }
     public void bGuess(View view){
         textView.setText("Угадал!");
